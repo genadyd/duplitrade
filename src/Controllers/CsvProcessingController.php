6aac13faf0aac0440d9bf7f2ed1751cf48bbc4e8
genady <jobgenady@gmail.com>
2021-03-13 22:19:22 +0200

    add memory check script

--- src/Controllers/CsvProcessingController.php
+++ src/Controllers/CsvProcessingController.php
@@ -51,6 +51,11 @@ final class CsvProcessingController
         $this->alerts_model->create($separator->getAlerts());
 
         echo  (microtime(true)-$start). "\n";
+        echo '<br>';
+        $memory_size = memory_get_usage();
+        $memory_unit = array('Bytes','KB','MB','GB','TB','PB');
+// Display memory size into kb, mb etc.
+        echo 'Used Memory : '.round($memory_size/pow(1024,($x=floor(log($memory_size,1024)))),2).' '.$memory_unit[$x]."\n";
 
     }
 
